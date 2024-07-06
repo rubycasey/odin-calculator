e077a3054cabb91df37421bcdb2bc5dcc31378df
Ruby Casey <ruby@cxsey.com>
2024-07-06 00:03:08 -0400

    Button Functions

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 // Elements
-let bttn0 = document.getElementById("bttn-0");
-let bttn1 = document.getElementById("bttn-1");
-let bttn2 = document.getElementById("bttn-2");
-let bttn3 = document.getElementById("bttn-3");
-let bttn4 = document.getElementById("bttn-4");
-let bttn5 = document.getElementById("bttn-5");
-let bttn6 = document.getElementById("bttn-6");
-let bttn7 = document.getElementById("bttn-7");
-let bttn8 = document.getElementById("bttn-8");
-let bttn9 = document.getElementById("bttn-9");
+let bttnNum = [
+    document.getElementById("bttn-0"),
+    document.getElementById("bttn-1"),
+    document.getElementById("bttn-2"),
+    document.getElementById("bttn-3"),
+    document.getElementById("bttn-4"),
+    document.getElementById("bttn-5"),
+    document.getElementById("bttn-6"),
+    document.getElementById("bttn-7"),
+    document.getElementById("bttn-8"),
+    document.getElementById("bttn-9"),
+];
+
 let bttnDeci = document.getElementById("bttn-decimal");
 
 let bttnClear = document.getElementById("bttn-AC");
@@ -27,13 +30,95 @@ let lastNum = 0;
 let currentNum = 0;
 let operator = null;
 let display = "0";
+let showingEval = false;
 
 // Init
 updateDisplay(display);
 
+// Button Functions
+// Digit Buttons
+bttnNum.forEach(function (currentBttn, i) {
+    currentBttn.addEventListener("click", () => {
+        if (showingEval) {
+            numberSwitch();
+        };
+        insertNumber(i);
+    });
+});
+bttnDeci.addEventListener("click", () => {
+        if (!currentNum.includes(".")) {
+            insertNumber(".");
+        };
+});
+
+// Function Buttons
+bttnClear.addEventListener("click", () => {
+    currentNum = 0;
+    lastNum = 0;
+    operator = null;
+    updateDisplay(currentNum);
+});
+bttnDel.addEventListener("click", () => {
+    currentNum = String(currentNum).slice(0, currentNum.length - 1);
+    updateDisplay(currentNum);
+});
+bttnEval.addEventListener("click", () => {
+    evaluate();
+    showingEval = true;
+});
+
+// Operator Buttons
+bttnAdd.addEventListener("click", () => {
+    operator = "+";
+    numberSwitch();
+});
+bttnSub.addEventListener("click", () => {
+    operator = "-";
+    numberSwitch();
+});
+bttnMult.addEventListener("click", () => {
+    operator = "*";
+    numberSwitch();
+});
+bttnDiv.addEventListener("click", () => {
+    operator = "/";
+    numberSwitch();
+});
+
 // Functions
+function evaluate() {
+    if (operator == null) {
+        console.log("No Operator")
+        return;
+    }
+    currentNum = operate(Number(lastNum), Number(currentNum), operator);
+    updateDisplay(currentNum);
+};
+
 function updateDisplay(text) {
+    // Updates "screen" on calculator
+    if (String(text) == "") {
+        text = 0;
+    }
     displayElement.innerText = text;
+    showingEval = false;
+};
+
+function insertNumber(number) {
+    // Inserts digits into current number
+    if (currentNum == 0) {
+        currentNum = "";
+    };
+    currentNum = String(currentNum + String(number));
+    updateDisplay(currentNum);
+};
+
+function numberSwitch() {
+    // Progresses number positions
+    if (!currentNum == 0) {
+            lastNum = currentNum;
+            currentNum = 0;
+        };
 };
 
 function operate(a, b, op) {
@@ -53,17 +138,25 @@ function operate(a, b, op) {
 };
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 };
 
 function subtract(a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 };
 
 function multiply(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 };
 
 function divide(a, b) {
-    return a / b;
+    return Number(a) / (b);
+};
+
+// For Debugging:
+function log() {
+    console.log(`last: ${lastNum}
+                \ncurrent: ${currentNum}
+                \noperator: ${operator}
+                \nshowingLast: ${showingEval}`);
 };
